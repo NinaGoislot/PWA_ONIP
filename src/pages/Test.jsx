4b5b5876a322332c5c2deda3b0ffb59b4dc1342d
NinaGoislot <ninagoislot.work@gmail.com>
2024-01-29 13:05:21 -0500

    v4

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,44 +1,38 @@
-import React, { useContext, useEffect, useState } from "react";
-import { GlobalContext } from '../App.jsx';
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 function Test() {
-    
     const [orientationData, setOrientationData] = useState({ alpha: 0, beta: 0, gamma: 0 });
 
-    function componentDidMount() {
+    const handleOrientation = (event) => {
+        const alpha = event.alpha;  // rotation autour de l'axe Z
+        const beta = event.beta;    // rotation autour de l'axe X
+        const gamma = event.gamma;  // rotation autour de l'axe Y
+
+        // Mettez à jour les données de l'orientation dans l'état local
+        setOrientationData({ alpha, beta, gamma });
+    };
+
+    useEffect(() => {
         if (window.DeviceOrientationEvent) {
             window.addEventListener('deviceorientation', handleOrientation);
         } else {
             console.error("DeviceOrientation n'est pas supporté");
         }
-    };
 
-    useEffect(() => {
-        /*if (window.DeviceOrientationEvent) {
-            window.addEventListener('deviceorientation', handleOrientation);
-        } else {
-            console.error("DeviceOrientation n'est pas supporté");
-        }*/
-
-    }, []);
-
-    function handleOrientation(event) {
-        const alpha = event.alpha;  // rotation autour de l'axe Z
-        const beta = event.beta;    // rotation autour de l'axe X
-        const gamma = event.gamma;  // rotation autour de l'axe Y
-
-        console.log(orientationData);
-
-        setOrientationData({ alpha, beta, gamma });
-    }
+        // Nettoyer l'écouteur d'événements lors du démontage du composant
+        return () => {
+            window.removeEventListener('deviceorientation', handleOrientation);
+        };
+    }, []); // Le tableau vide signifie que cela ne s'exécutera qu'une fois à l'initialisation
 
     return (
         <main className="h-screen w-screen flex flex-col justify-around item-center bg-slate-700">
             <div className="flex justify-center items-center h-full">
-                    <Link to="/"><button className="bg-slate-400 h-20 p-8">Retourner a l'accueil </button></Link>
+                <Link to="/"><button className="bg-slate-400 h-20 p-8">Retourner à l'accueil</button></Link>
             </div>
             <div className="flex flex-col bg-slate-300 h-fit">
+                {/* Afficher les données de l'orientation en permanence */}
                 {orientationData && (
                     <div>
                         <p>Alpha: {orientationData.alpha}</p>
@@ -48,6 +42,7 @@ function Test() {
                 )}
             </div>
         </main>
-    )
+    );
 }
+
 export default Test;
