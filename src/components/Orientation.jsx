7bf534a30a58f05777655453b32397e323281c94
NinaGoislot <ninagoislot.work@gmail.com>
2024-01-29 14:14:32 -0500

    v12

---
+++ src/components/Orientation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Orientation() {
+
+    return (
+        <main className="h-screen w-screen flex flex-col justify-around item-center bg-slate-700">
+           <p>okay</p>
+        </main>
+    );
+}
+
+export default Orientation;
