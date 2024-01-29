0a7792895ac0e910eeae33f04a37acb8572342cf
NinaGoislot <ninagoislot.work@gmail.com>
2024-01-29 15:04:17 -0500

    v18

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
-import Orientation from "../components/Orientation";
 
 function Test() {
-    const [orientationData, setOrientationData] = useState({ alpha: 6, beta: 0, gamma: 0 });
+    const [orientationData, setOrientationData] = useState({ alpha: 0, beta: 0, gamma: 0 });
 
     const handleOrientation = (event) => {
         console.log(event);
-        const alpha = 3;  // rotation autour de l'axe Z
+        const alpha = event.alpha;  // rotation autour de l'axe Z
         const beta = event.beta;    // rotation autour de l'axe X
         const gamma = event.gamma;  // rotation autour de l'axe Y
 
@@ -36,7 +35,16 @@ function Test() {
             <div className="flex justify-center items-center h-full">
                 <Link to="/"><button className="bg-slate-400 h-20 p-8">Retourner à l'accueil</button></Link>
             </div>
-            <Orientation/>
+            <div className="flex flex-col bg-slate-300 h-fit">
+                {/* Afficher les données de l'orientation en permanence */}
+                {orientationData && (
+                    <div>
+                        <p>Alpha: {orientationData.alpha}</p>
+                        <p>Beta: {orientationData.beta}</p>
+                        <p>Gamma: {orientationData.gamma}</p>
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
