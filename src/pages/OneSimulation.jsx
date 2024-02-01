798b76dd2a3c96961e52b9b306b567961b9a9afa
NinaGoislot <ninagoislot.work@gmail.com>
2024-02-01 13:00:39 -0500

    v45 : Passer en revue les directions du tableau

--- src/pages/OneSimulation.jsx
+++ src/pages/OneSimulation.jsx
@@ -186,7 +186,7 @@ function OneSimulation() {
     /******************************************************************** Code HTML ********************************************************************/
     return (
         <main className="w-screen h-screen flex flex-col gap-4 bg-slate-700 p-4 justify-center items-center">
-            <h1 className='text-2xl font-bold text-white text-center'>Simulation du mouvement {movement.id}</h1>
+            <h1 className='text-2xl font-bold text-green-500 text-center'>Simulation du mouvement {movement.id}</h1>
             <p className='text-center italic text-sm text-white'>
                 Évaluation portée sur {movement.timer ? 'le nombre de coups réalisés' : 'la précision du mouvement'}
             </p>
