b6c9e23dd41fba08322f1395db6e1f4c0cfba3ab
NinaGoislot <ninagoislot.work@gmail.com>
2024-02-06 09:42:40 -0500

    v52 : gestion de l'orientation → utilisation d'un useEffect

--- src/pages/OneSimulation.jsx
+++ src/pages/OneSimulation.jsx
@@ -128,24 +128,18 @@ function OneSimulation() {
         };
     }, [isTimerRunning, timerMovement]);
 
-
-    /******************************************************************** Fonctions ********************************************************************/
-    const closeModal = () => setShowResults(false);
-    const handleMotion = (event) => {
-        const { acceleration, rotationRate } = event;
-        setMotionData({ acceleration, rotationRate });
-
+    //Pour gérer le mouvement selon l'orientation
+    useEffect(() => {
+        
         /**************** Logique des directions ****************/
         const threshold = movement.thershold_general;  // Seuil pour considérer un mouvement significatif
         let currentDirection = "None";  // Variable d'état pour suivre la direction actuelle
 
-
-        // Logique de traitement pour les diagonales
         if (Math.abs(acceleration.x) > threshold && Math.abs(acceleration.y) > threshold) {
-            console.log("Je rentre dans le if, orientation vaut "+ orientation )
+            console.log("Je rentre dans le if, orientation vaut " + orientation)
             switch (orientation) {
                 case "N":
-                console.log("Je rentre dans le switch");
+                    console.log("Je rentre dans le switch");
 
                     if (Math.abs(acceleration.x) > Math.abs(acceleration.y)) {
                         currentDirection = acceleration.x > 0 ? "Ouest" : "Est";
@@ -163,6 +157,7 @@ function OneSimulation() {
                 case "NE":
                     if (Math.abs(acceleration.x) > Math.abs(acceleration.y)) {
                         currentDirection = acceleration.x > 0 ? "Nord-Ouest" : "Sud-Est";
+                        0
                     } else {
                         currentDirection = acceleration.y > 0 ? "Sud-Ouest" : "Nord-Est";
                     }
@@ -268,6 +263,14 @@ function OneSimulation() {
         if (currentDirection !== "None") {
             setDirection(currentDirection);
         }
+    }, [motionData, orientation]);
+
+
+    /******************************************************************** Fonctions ********************************************************************/
+    const closeModal = () => setShowResults(false);
+    const handleMotion = (event) => {
+        const { acceleration, rotationRate } = event;
+        setMotionData({ acceleration, rotationRate });
     };
 
     const handleOrientation = (event) => {
@@ -343,7 +346,7 @@ function OneSimulation() {
 
             <div className={`${showResults ? 'absolute h-screen w-screen bg-black opacity-50' : 'hidden'}`}></div>
 
-            <h1 className='text-2xl font-bold text-orange-500 text-center'>Simulation du mouvement {movement.id}</h1>
+            <h1 className='text-2xl font-bold text-purple-500 text-center'>Simulation du mouvement {movement.id}</h1>
             <p className='text-center italic text-sm text-white'>
                 Évaluation portée sur {timerMovement != 0 ? 'le nombre de coups réalisés' : 'la précision du mouvement'}
             </p>
