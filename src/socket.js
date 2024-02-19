d70499d9d4c94bff47d5893e5fdd166980e4924b
NinaGoislot <ninagoislot.work@gmail.com>
2024-02-19 13:59:59 -0500

    Implémentation ml5 : v4

--- src/socket.js
+++ src/socket.js
@@ -3,5 +3,5 @@ import { io } from "https://cdn.socket.io/4.7.3/socket.io.esm.min.js";
 
 // Local : export const socket = io("http://127.0.0.1:3006");
 // Nina Cégep : 
-export const socket = io("http://10.1.180.126:3006");
+export const socket = io("https://10.1.180.126:3006");
 // Nina Bloc : export const socket = io("https://192.168.30.233:3006");
