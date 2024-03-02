885104b50c36be217b971a0e2304d1cf7dbd100f
NinaGoislot <ninagoislot.work@gmail.com>
2024-03-01 22:56:14 -0500

    Ajout des nouveaux visuels

--- src/pages/Wait.jsx
+++ src/pages/Wait.jsx
@@ -3,7 +3,6 @@ import { GlobalContext } from '@/App.jsx';
 import { Link } from 'react-router-dom';
 import { socket } from "@/socket";
 import { useNavigate } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
 //import { Swipeable } from 'react-swipeable';
 //import {SwipeableViews} from 'react-swipeable-views-react-18-fix';
 
@@ -12,7 +11,7 @@ import 'aos/dist/aos.css';
 
 function Wait() {
 
-    const [canMove, setCanMove] = useState(false);
+    const [canMove, setCanMove] = useState(true);
     const [startX, setStartX] = useState(null);
     const [endX, setEndX] = useState(null);
     const swipeThreshold = 150;
@@ -90,14 +89,14 @@ function Wait() {
                         style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
                     >
                         <div className="relative h-full w-full flex flex-col justify-between items-center">
-                            <img data-aos="fade-left" className="w-fit block h-[10%] rotate-180" src="/PWA/pictures/fleche-swipe.webp" alt="" />
+                            <img data-aos="fade-left" className="w-fit block h-[10%]" src="/PWA/pictures/fleche-swipe2.webp" alt="Swipe pour accéder à l'amoire à bouteilles" />
 
                             <img data-aos="fade-left" className="" src="/PWA/pictures/txt-balaie.webp" alt="" />
                             {/* <div className="flex flex-col justify-between items-center w-full h-full absolute z-[-1]">
                                 <img className="w-fit block h-[10%] transform scale-x-[-1]" src="/PWA/pictures/fleche-swipe.webp" alt="" />
                                 <img className="w-fit block h-[10%]" src="/PWA/pictures/fleche-swipe.webp" alt="" />
                             </div> */}
-                            <img data-aos="fade-right" className="w-fit block h-[10%]" src="/PWA/pictures/fleche-swipe.webp" alt="" />
+                            <img data-aos="fade-right" className="h-[10%]" src="/PWA/pictures/fleche-swipe.webp" alt="Swipe pour accéder à l'amoire à bouteilles" />
 
                         </div>
                     </div>
